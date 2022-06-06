e0a25f6e4ac08a6bb650f39bedff75ee74a3a919
Abhijeet Mishra <abhijeetmishra0408@gmail.com>
2022-06-07 00:33:53 +0530

    test

--- shopify_admin/settings.py
+++ shopify_admin/settings.py
@@ -78,14 +78,27 @@ WSGI_APPLICATION = 'shopify_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT':  os.environ.get("DATABASE_PORT"),
     }
 }
 
-
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get("DATABASE_URL")), 
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',}
+# DATABASES['default'] =  dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
